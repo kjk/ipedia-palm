06863f29e4024fb7cb13f896fc80892a01fe7984
andrzejc <andrzejc@38655da0-86da-0310-9534-e79ea18dc88d>
2004-05-17 06:35:13 +0000

    removed bunch of shared files from iPedia - these live in ars_framework
    git-svn-id: https://repo.kowalczyk.info/svn/ipedia/trunk@282 38655da0-86da-0310-9534-e79ea18dc88d

--- ipedia/Src/ParagraphElement.cpp
+++
@@ -1,7 +0,0 @@
-#include "ParagraphElement.hpp"
-
-void IndentedParagraphElement::calculateLayout(LayoutContext& layoutContext)
-{
-    setChildIndentation(layoutContext.preferences.standardIndentation());
-    ParagraphElement::calculateLayout(layoutContext);
-}
