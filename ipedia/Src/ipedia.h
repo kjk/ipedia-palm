7d3614eb4ba565b2a8691e8a3c2ca38cc3060498
kjk <kjk@38655da0-86da-0310-9534-e79ea18dc88d>
2005-01-16 06:30:50 +0000

    set svn:ignore
    git-svn-id: https://repo.kowalczyk.info/svn/ipedia/trunk@555 38655da0-86da-0310-9534-e79ea18dc88d

--- ipedia/Src/ipedia.h
+++ ipedia/Src/ipedia.h
@@ -5,7 +5,6 @@
 
 #define appFileCreator          'iPED'
 #define appName                 _T("iPedia")
-#define appVersionNum           0x01
 #define appPrefID               0x00
 #define appPrefVersionNum       0x01
 
