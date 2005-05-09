45c52933c42b482795acf535ca90ef20b8dfbc7f
kjk <kjk@38655da0-86da-0310-9534-e79ea18dc88d>
2005-05-09 08:53:13 +0000

    added scripts
    git-svn-id: https://repo.kowalczyk.info/svn/ipedia/trunk@605 38655da0-86da-0310-9534-e79ea18dc88d

---
+++ ipedia/scripts/createlocal.bat
@@ -0,0 +1,20 @@
+@rem set settings based on computer name
+@if %computername%==DVD goto SetDVD
+@if %computername%==DVD2 goto SetDVD
+@if %computername%==KJKLAP1 goto SetDVD
+
+@echo "Don't know the setup for computer %computername%"
+@goto EOF
+
+:SetDVD
+@SET DSTDIR=c:\kjk\ipedia\src
+@SET SRCDIR=c:\kjk\src\mine\ipedia
+@goto SetupDone
+
+:SetupDone
+
+@del /f /s /q %DSTDIR%
+@mkdir %DSTDIR%
+@copy %SRCDIR%\server\*.py %DSTDIR%
+@copy %SRCDIR%\scripts\*.py %DSTDIR%
+@copy %SRCDIR%\scripts\reg_codes.csv %DSTDIR%
---
+++ ipedia/scripts/runlocal.bat
@@ -0,0 +1,24 @@
+@rem create a local copy 
+
+@rem set settings based on computer name
+@if %computername%==DVD goto SetDVD
+@if %computername%==DVD2 goto SetDVD
+@if %computername%==KJKLAP1 goto SetDVD
+
+@echo "Don't know the setup for computer %computername%"
+@goto EOF
+
+:SetDVD
+@SET DSTDIR=c:\kjk\ipedia\src
+@SET SRCDIR=c:\kjk\src\mine\ipedia
+@goto SetupDone
+
+:SetupDone
+
+@call createlocal.bat
+
+pushd %DSTDIR%
+start iPediaServer.py -verbose -disableregcheck %1 %2 %3
+popd
+
+:EOF
